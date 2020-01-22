3e124d017449404b7de8a1302ffbe33a3eaad39b
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-22 00:03:57 +0000

    Added login/logout functionality which stores a username in a cookie upon login and deletes the cookie upon logout

--- express_server.js
+++ express_server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -23,19 +25,36 @@ function generateRandomString() {
   return result;
 }
 
+//login
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+//logout
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
+// redirect from short URL to the assigned long URL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 // create new short and long URL pair
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/", (req, res) => {
@@ -44,9 +63,10 @@ app.post("/urls/", (req, res) => {
   res.redirect(`/urls/${newShortURL}`);
 });
 
-// get short:long URL pair
+// read url
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
+    username: req.cookies["username"],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
@@ -66,6 +86,7 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls/");
 });
 
+//catchall for errors when routing incorrect urls
 app.get("*", (req, res) => {
   res.redirect("/");
 });
