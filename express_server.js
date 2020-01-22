9fd6fa2bbe4f4d508048a6ba028ce83cc5d750bb
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-01-22 21:30:03 +0000

    Tested all user cases for user registration feature successfully

--- express_server.js
+++ express_server.js
@@ -121,7 +121,7 @@ app.post("/login", (req, res) => {
 //logout
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 // redirect from short URL to the assigned long URL
